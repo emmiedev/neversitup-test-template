ba86d0dd777c87ed8730d1fbfda2a7eb59f2d0c4
tawatchaiinsree <tawatchaiinsree@gmail.com>
2024-01-17 20:10:34 +0700

    Successfully completed the manipulate.js test.

--- javascript/manipulate.js
+++ javascript/manipulate.js
@@ -3,8 +3,44 @@
  * @param {string} text string of value example "aabb", "abcde"
  * @returns {string[]} string array of result
  */
+
 const manipulate = (text) => {
     // TODO : start your code here
+    if (text.length === 0) {
+        return []
+    } else {
+        let result = []
+        let arr = text.split('').sort()
+        let temp = []
+
+        const swap = (arr, a, b) => {
+            let temp = arr[a]
+            arr[a] = arr[b]
+            arr[b] = temp
+        }
+
+        const permute = (arr, start, end) => {
+            if (start === end) {
+                temp.push(arr.join(''))
+            } else {
+                for (let i = start; i <= end; i++) {
+                    swap(arr, start, i)
+                    permute(arr, start + 1, end)
+                    swap(arr, start, i)
+                }
+            }
+        }
+
+        permute(arr, 0, arr.length - 1);
+
+        for (let i = 0; i < temp.length; i++) {
+            if (!result.includes(temp[i])) {
+                result.push(temp[i])
+            }
+        }
+        
+        return result.sort()
+    }
 }
 
 module.exports = {
--- javascript/manipulate.spec.js
+++ javascript/manipulate.spec.js
@@ -1,1 +1,21 @@
 // Write your test here
+const { manipulate } = require('./manipulate')
+
+describe('manipulate', () => {
+    test('should return ["a"] when text is "a"', () => {
+        expect(manipulate('a')).toEqual(["a"])
+    })
+    test('should return ["ab", "ba"] when text is "ab"', () => {
+        expect(manipulate("ab")).toEqual(["ab", "ba"])
+    })
+    test('should return ["abc","acb","bac","bca","cab","cba"] when text is "abc"', () => {
+        expect(manipulate("abc")).toEqual(["abc","acb","bac","bca","cab","cba"])
+    })
+
+    test('should return ["aabb", "abab", "abba", "baab", "baba", "bbaa"] when text is "aabb"', () => {
+        expect(manipulate("aabb")).toEqual(["aabb", "abab", "abba", "baab", "baba", "bbaa"])
+    })
+    test('should return [] when text is ""', () => {
+        expect(manipulate('')).toEqual([])
+    })
+})
